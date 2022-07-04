8fb139a31a6f4b5b2874997ec1e0dfce97af732b
Oleh Khrystofor <hristoforolegivanovich@gmail.com>
2022-07-05 00:29:30 +0300

    add

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+ 
+    <div class="nav container">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+  
+  
   <router-view/>
 </template>
 
@@ -20,16 +23,20 @@
   @include letterH4HeadingBurgerMenuTextNumbersDarkBlue;
 }
 
-nav {
+.nav {
   padding: 30px;
+  display: flex;
+  justify-content: center;
+  border: 1px solid black;
 }
 
-nav a {
+.nav a {
   font-weight: bold;
   color: #2c3e50;
+  
 }
 
-nav a.router-link-exact-active {
+.nav a.router-link-exact-active {
   color: #42b983;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Header One</h1>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'header-one',
+  
+}
+</script>
+
+
+<style lang="scss" scoped>
+  @import '../assets/varmix.scss';
+
+
+</style>
