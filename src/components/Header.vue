f82a800b188e011f22121a7f9f2297f6787cf345
Oleh Khrystofor <hristoforolegivanovich@gmail.com>
2022-07-06 16:06:56 +0300

    plus

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,22 +1,61 @@
 <template>
-  <div class="container">
-    <h1>Header!</h1>
-    <burger/>
+  <div class="container mycontainer">
+    <h1 ref="tex">Header!</h1>
+    <burger v-bind:class="{burgerCross:isCross}"  v-on:click="isCross=!isCross"   />
   </div>
 </template>
 
 <script>
-  import burger from './primitives/BurgerBtn.vue'
+  
+import burger from './primitives/BurgerBtn.vue'
   export default {
     name: 'Header-one',
     components:{
       burger,
-    }
+    },
+    data () {
+    return {
+      
+      isCross:false,
+      
+       
+    }},
 
+    watch: {
+      isCross:function (val) {
+          if(val) {
+            this.$refs.tex.style.display = 'none'
+          }
+          else {
+            this.$refs.tex.style.display = 'block'
+          }
+      }
+    }
+    
+    
   }
+
+    
+
+  
 </script>
 
 
 <style lang="scss" scoped>
   @import '../assets/varmix.scss';
+
+  .mycontainer {
+    position: relative;
+    padding-top: 18px;
+    padding-bottom: 18px;
+  }
+
+  h1 {
+    position: absolute;
+    top:0;
+    left: 0;
+  }
+  
+  
+
 </style>
--- src/components/primitives/BurgerBtn.vue
+++ src/components/primitives/BurgerBtn.vue
@@ -1,7 +1,18 @@
 <template>
-  <div class="burger">
-        
-    
+  <div class="burgerLines" >
+
+    <div class="topLine">
+
+    </div> 
+
+    <div class="cntLine">
+
+    </div>  
+
+    <div class="botLine">
+
+    </div> 
+
   </div>
 </template>
 
@@ -10,12 +21,93 @@
 export default {
   name: 'Burger-menu',
   
+  data () {
+
+  }
+  
 }
 </script>
 
 
 <style lang="scss" scoped>
   @import '../../assets/varmix.scss';
+    .burgerLines{
+    display: inline-block;
+    position: relative;
+    height: $sizeBurgerLineWidthFullScreen;
+    width: $sizeBurgerLineWidthFullScreen;
+    position: absolute;
+    top:17px;
+    right: 0;
+        .topLine {
+            position: absolute;
+            width: $sizeBurgerLineWidthFullScreen;
+            height: $sizeBurgerLineHeightFullScreen;
+            background-color: $colorTitleBurgerBlueDark;
+            top:0;
+           
+        }
+        .cntLine {
+            display: inline-block;
+            width: $sizeBurgerLineWidthFullScreen;
+            height: $sizeBurgerLineHeightFullScreen;
+            background-color: $colorTitleBurgerBlueDark;
+            position: absolute;
+            top:50%;
+            transform: translateY(-50%);
+        }   
+        .botLine{
+          
+            position: absolute;
+            width: $sizeBurgerLineWidthFullScreen;
+            height: $sizeBurgerLineHeightFullScreen;
+            background-color: $colorTitleBurgerBlueDark;
+            bottom: 0;
+           
+          }
+    }
 
+    
+    .burgerCross {
+      display: inline-block;
+      position: relative;
+      height: $sizeBurgerLineWidthFullScreen;
+      width: $sizeBurgerLineWidthFullScreen;
+      position: absolute;
+      top:17px;
+      right: 0;
+        .topLine {
+            position: absolute;
+            width: $sizeBurgerLineWidthFullScreen;
+            height: $sizeBurgerLineHeightFullScreen;
+            background-color: $colorTitleBurgerBlueDark;
+            top:50%;
+            transform: translateY(-50%);
+            transform: rotate(135deg);
+           
+        }
+        .cntLine {
+            display: inline-block;
+            width: $sizeBurgerLineWidthFullScreen;
+            height: $sizeBurgerLineHeightFullScreen;
+            background-color: $colorTitleBurgerBlueDark;
+            position: absolute;
+            top:50%;
+            transform: translateY(-50%);
+            opacity: 0;
+        }   
+        .botLine{
+          
+            position: absolute;
+            width: $sizeBurgerLineWidthFullScreen;
+            height: $sizeBurgerLineHeightFullScreen;
+            background-color: $colorTitleBurgerBlueDark;
+            top:50%;
+            transform: translateY(-50%);
+            transform: rotate(45deg);
+           
+          }
+    }
+   
 
 </style>
