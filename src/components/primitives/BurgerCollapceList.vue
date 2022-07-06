a73ecf1edb5364037f7832bd82e82890417316fe
Oleh Khrystofor <hristoforolegivanovich@gmail.com>
2022-07-06 17:31:38 +0300

    plus

---
+++ src/components/primitives/BurgerCollapceList.vue
@@ -0,0 +1,51 @@
+<template>
+  <ul>
+    <li v-for="(menuitem,id) in burgerMenuList" :key="id">
+        <router-link :to="menuitem.path">
+            {{menuitem.name}}
+        </router-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+  
+
+  export default {
+    name: 'BurgerCollapce-list',
+   
+    data () {
+    return {
+      
+        burgerMenuList: [
+            {name:'Home', path:'/'},
+            {name:'Contact us', path:'/contact'},
+            {name:'About VAC', path:'/about'},
+            {name:'Loan Rates', path:'/loan'},
+            {name:'Video', path:'/video'},
+            {name:'Blog', path:'/blog'},
+            {name:'Calculate', path:'/calculate'},
+        ]
+      
+       
+    }},
+
+   
+    
+    
+  }
+
+    
+
+  
+</script>
+
+
+<style lang="scss" scoped>
+  @import '../assets/varmix.scss';
+
+ 
+  
+  
+
+</style>
