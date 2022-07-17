f3256f9b3c043528493f004d253d3e69ec8faece
Oleh Khrystofor <hristoforolegivanovich@gmail.com>
2022-07-17 19:47:46 +0300

    Please

--- src/components/HOME/ChooseHomeP.vue
+++ src/components/HOME/ChooseHomeP.vue
@@ -3,24 +3,28 @@
 
                     <div class="row">
                         <div class="col choose">
-                            <div class="choose__carImgBox col-md-12 ">
+                            <div class="choose__carImgBox col-md-6 ">
                                 <img src="../../assets/images/Corola1.png" class="choose__carImgBox_imgCor"  alt="corola">
                                 <img src="../../assets/images/HondaCRV.png" class="choose__carImgBox_imgHon" alt="hondaCRV">
                             </div>
 
-                            <div class="choose__textBox col-md-12">
+                            <div class="choose__textBox col-md-6">
 
                                 <div class="choose__heading">
                                     <h2darkblue>Сhoose your dream vehicle in our catalog</h2darkblue>
                                 </div>
 
-                                <div class="choose__textRegular">
-                                    <textregular>In our catalogue there are many models of vehicles. Choose your car to your liking.</textregular>
+                                <div class="choose__textBTNbox">
+                                    <div class="choose__textRegular">
+                                         <textregular>In our catalogue there are many models of vehicles. Choose your car to your liking.</textregular>
+                                    </div>
+
+                                    <div class="choose__btn">
+                                        <btnbluetextwite class="choose__BtnComp">Inventory</btnbluetextwite>
+                                    </div>
                                 </div>
 
-                                <div class="choose__btn">
-                                    <btnbluetextwite>Inventory</btnbluetextwite>
-                                </div>
+                                
 
                             </div>
 
@@ -84,30 +88,81 @@
             border-width:2px;
             border-style: solid;
             border-color: #000;
+            position: relative;
+            @media (max-width:768px) {
+               width: 100%;
+               height: 20vw;
+               order:2;
+            }
             &_imgCor{
-                margin-left:calc(100vw * #{$adaptiveMarLeftChoose});
+                position:absolute;
+                top: 40%;
+                left: 60%;
+                transform: translate(-50%,-50%);
                 width: calc(100vw * #{$adaptiveCorolaChooseWidth});
                 height: calc(100vw * #{$adaptiveCorolaChooseHeight});
-                object-fit: cover;
+                max-width: 428px;
+                max-height: 187px;
+                
+           
             }
             &_imgHon{
-                margin-top:calc(100vw * #{$adaptiveMarTopChoose});
+                position:absolute;
+                top: 60%;
+                left:Min(300px,calc(100vw * #{$adaptiveMarLeftChoose}));
+                transform: translate(-50%,-50%);
                 width: calc(100vw * #{$adaptiveHondaChooseWidth});
                 height: calc(100vw * #{$adaptiveHondaChooseHeight});
-                object-fit: cover;
+                max-width: 491px;
+                max-height: 234px;
+                @media (max-width:768px) {
+                   left: 40%;
+                }
             }
 		}
 
 		&__textBox {
-            height: 300px;
+           
             width: 50%;
             border-width:2px;
             border-style: solid;
             border-color: #000;
+            @media (max-width:768px) {
+               width: 100%;
+               display: flex;
+               order:1;
+            }
+            @media (max-width:576px) {
+               flex-direction: column;
+            }
 		}
 
+        &__textBTNbox {
+           @media (max-width:768px) {
+              padding-left: 10px;
+           }
+           @media (max-width:576px) {
+              padding-left: 0px;
+           }
+        }
+
 		&__heading {
+            @media (max-width:768px) {
+               padding-right: 10px;
+            }
+            @media (max-width:576px) {
+               padding-right: 0px;
+            }
+            @media (max-width:992px) {
+               font-size: 2rem;
+            }
+            @media (max-width:420px) {
+               font-size: 1.5rem;
+               line-height: 1.8rem;
+            }
 		}
+
+        
 
 		&__textRegular {
 		}
---
+++ src/components/primitives/BTN/btnBlueTextWhiteSlotMob100.vue
@@ -0,0 +1,66 @@
+<template>
+   
+            <div class="col">
+                <div class="btnBlue">
+                    <span class="btnBlue__text"><slot></slot></span>
+
+                </div>
+            
+            </div>
+       
+  
+</template>
+
+<script>
+
+
+
+  export default {
+    
+    name: 'Btn-blue',
+    
+  }
+
+</script>
+
+
+<style lang="scss" scoped>
+  @import '../../../assets/varmix.scss';
+
+  .btnBlue {
+    position: relative;
+    border-width:2px;
+    border-style: solid;
+    border-color: $colorMainBlue;
+    background-color:$colorMainBlue;
+    @include letterBtnWhiteText ;
+    border-radius: 2px;
+    width: 200px;
+    height: 45px;
+    white-space: nowrap;
+    @media (max-width:576px) {
+       width: 100%;
+    }
+    &:hover {
+        background-color: rgb(0, 162, 255);
+    }
+
+		&__text {
+            text-transform:uppercase ;
+            position:absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%,-50%);
+		}
+}
+
+  
+
+  
+
+  
+
+  
+
+  
+</style>
