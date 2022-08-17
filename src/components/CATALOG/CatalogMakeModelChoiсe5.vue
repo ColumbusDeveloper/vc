e4e57580775a44ada9c7953459089596d1a6e5a3
Oleh Khrystofor <hristoforolegivanovich@gmail.com>
2022-08-18 02:01:48 +0300

    Please

--- src/components/CATALOG/CatalogMakeModelChoiсe5.vue
+++ src/components/CATALOG/CatalogMakeModelChoiсe5.vue
@@ -2,8 +2,8 @@
     <div class="make-model">
 
         <div class="make-model__btn-arrow-box">
-            <div class="make-model__nameBtn" @click="form=!form,getID()" >Make, Model</div>
-            <div class="make-model__arrow-box" @click="form=!form,getID()">
+            <div class="make-model__nameBtn" @click="form=!form,getStarted()" >Make, Model</div>
+            <div class="make-model__arrow-box" @click="form=!form,getStarted()">
                 <div class="make-model__arrow-Up"><i class="fa-solid fa-angle-up" v-if="form"></i></div>
                 <div class="make-model__arrow-Down"><i class="fa-solid fa-angle-down" v-if="form===false"></i></div>
             </div>
@@ -15,19 +15,21 @@
             <div class="make-model__form-show-box">
                 
                     <div class="make-model__choice-form"
-                    v-for="(value, name, index) in object" :key="index"
+                    v-for="(value, name, index) in object" :key="index" 
                     >
                     
                        
                         <input type="checkbox" v-bind:value="name" 
-                        v-model="makes"     
-                        @change="getStatus()"
+                        v-model="originalMakes"     
+                        @change="deleteAddItemFromMakes(name)"
                         
                         >
-                        <label class="make-model__label">{{name}}</label>
-                        {{getModels(name)}}
+
+                        <label class="make-model__label">{{name}}{{findModels(name)}}</label>
+                    
                             <div class="make-model__choice-form-models"
-                            v-for="item in ModelsFromComp" :key="item"
+                            v-for="item in findModelsComp" :key="item"
+                            @change="deleteAddItemFromModels(name,item)"
                             >
                                 <input type="checkbox" v-bind:value="item" v-model="models">
                                 <label class="make-model__label">{{item}}</label>
@@ -83,10 +85,12 @@
                 cars: cars,
                 form: false,
                 originalMakes:[],
-                makes:[], 
-                models:[],
+                makes:[],
                 varModels:[],
-             
+                models:[], 
+              
+
+
                 object: {
                 Porsche: 'Porsche',
                 Audi: 'Audi',
@@ -101,12 +105,75 @@
         },
         methods: {
             
-           getModels(newVal) {
-            this.varModels = newVal
+            getStarted() {
+                
+                for (let i = 0; i<this.cars.length; i++) {
+                let a = this.cars[i].make
+                let b = this.cars[i].model
+                this.models.push(b)
+                this.models = [...new Set(this.models)]  
+                this.originalMakes.push(a)
+                this.originalMakes = [...new Set(this.originalMakes)]        
+                }
+            },
+
+           
+           deleteAddItemFromMakes(val) {
+                let b = val
+                let z = this.makes.findIndex(el=>el===b)
+                console.log(z);
+                let s = this.cars.filter(el=>el.make.indexOf(b)>-1)
+                if (s!==[]) {
+                    
+                    for (let i = 0; i<s.length; i++) {
+                        let m = s[i].model
+                        let g = this.models.findIndex(el=>el===m)
+             
+                            if (g!=-1) { 
+                                this.models.splice(g,1)
+                                this.originalMakes.splice(z,0,1)
+                               
+                            } else {
+                                this.models.splice(g,0,m)
+                                this.originalMakes.splice(z,0,1)
+                            }                    
+                    }
+                }
+            },
+
+           deleteAddItemFromModels(make,model) {
+                let a = make
+                let f = model
+                let d = this.models.includes(f)
+                if (d) {
+                    this.originalMakes.push(a)
+                    this.originalMakes = [...new Set(this.originalMakes)]
+                }    else {
+                    let w = this.originalMakes.findIndex(el=>el===a)
+                    if (w>-1) {
+                        this.originalMakes.splice(w,1)
+                    }
+                  
+                    console.log(w); 
+                }
+                                         
            },
-           getStatus() {
-            
-           }
+
+
+           findModels(valCar) {
+                this.varModels = valCar        
+            },
+
+
+                               
+
+
+
+
+
+
+
+           
                                    
         },
         created(){
@@ -114,14 +181,16 @@
             for (let i = 0; i<this.cars.length; i++) {
             let a = this.cars[i].make
             let b = this.cars[i].model
-            this.makes.push(a)
-            this.makes = [...new Set(this.makes)]     
             this.models.push(b)
             this.models = [...new Set(this.models)]  
-               
+            this.originalMakes.push(a)
+            this.originalMakes = [...new Set(this.originalMakes)]  
+            this.makes.push(a)
+            this.makes = [...new Set(this.makes)]           
             }
-           
-                       
+
+            
+                    
         },
         watch: {
 
@@ -132,8 +201,7 @@
 
 
         computed: {
-           
-            ModelsFromComp () { 
+           findModelsComp () { 
                 let m = []         
                 let a = this.varModels  
                 let b = this.cars.filter(el=>el.make.indexOf(a)>-1)
@@ -144,6 +212,7 @@
                 m = [...new Set(m)]
                 return m
             },
+            
 
         }
 
