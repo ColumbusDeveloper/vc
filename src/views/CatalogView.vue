eece9324241bb54c7f30d02a4fae7b050c4e3a12
Oleh Khrystofor <hristoforolegivanovich@gmail.com>
2022-09-15 00:34:52 +0300

    Please

--- src/views/CatalogView.vue
+++ src/views/CatalogView.vue
@@ -38,17 +38,17 @@ const car = (id, make, model, bodytype, transmission, price, year, kilometers, i
 })
 
 const cars = [
-    car(1, 'NISSAN', 'L-200', 'Trucks', 'Automatic', 15000, 2012, 30000, 'Mers.png'),
+    car(1, 'NISSAN', 'L-200', 'Trucks', 'Automatic', 15300, 2012, 30000, 'Mers.png'),
     car(2, 'NISSAN', 'L-200', 'Trucks', 'Automatic', 12000, 2010, 50000, 'Mers.png'),
-    car(3, 'MITSUBISHI', 'Outlander', 'SUV', 'Automatic', 12000, 2014, 90000, 'Mers.png'),
+    car(3, 'MITSUBISHI', 'Outlander', 'SUV', 'Automatic', 12400, 2014, 90000, 'Mers.png'),
     car(4, 'MITSUBISHI', 'Outlander', 'SUV', 'Manual', 10000, 2009, 110000, 'Mers.png'),
-    car(5, 'FORD', 'Mondeo', 'Sedan', 'Automatic', 20000, 2013, 150000, 'Mers.png'),
-    car(6, 'FORD', 'Mondeo', 'Sedan', 'Manual', 10000, 2005, 200000, 'Mers.png'),
+    car(5, 'FORD', 'Mondeo', 'Sedan', 'Automatic', 20800, 2013, 150000, 'Mers.png'),
+    car(6, 'FORD', 'Mondeo', 'Sedan', 'Manual', 10500, 2005, 200000, 'Mers.png'),
     car(7, 'PORSCHE', 'Porsche-911', 'Sedan', 'Automatic', 60000, 2011, 33000,'Mers.png'),
-    car(8, 'PORSCHE', 'Panamera', 'Hatchback', 'Automatic', 30000, 2017, 36000,'Mers.png'),
+    car(8, 'PORSCHE', 'Panamera', 'Hatchback', 'Automatic', 32000, 2017, 36000,'Mers.png'),
     car(9, 'SKODA', 'Octavia', 'Hatchback', 'Manual', 12000, 2018, 15000, 'Mers.png'),
     car(10, 'HONDA', 'Accord', 'Coupe', 'Automatic', 11000, 2015, 38000, 'Mers.png'),
-    car(11, 'MINI', 'Cabrio', 'Convertiable', 'Automatic', 15000, 2019, 40000, 'Mers.png'),
+    car(11, 'MINI', 'Cabrio', 'Convertiable', 'Automatic', 15800, 2019, 40000, 'Mers.png'),
     car(12, 'OPEL', 'Vivaro', 'VAN', 'Manual', 9000, 2008, 45000, 'Mers.png'),
     car(13, 'MERCEDES', 'Viano', 'VAN', 'Automatic', 27000, 2016, 125000, 'Mers.png'),
     car(14, 'VAS', 'Niva', 'Trucks', 'Manual', 500, 2003, 100000, 'Mers.png'),
