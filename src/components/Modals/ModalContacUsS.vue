ae9762e36843feb4039976a9ccbc777959355982
Oleh Khrystofor <hristoforolegivanovich@gmail.com>
2022-10-09 16:42:47 +0300

    pl

---
+++ src/components/Modals/ModalContacUsS.vue
@@ -0,0 +1,61 @@
+<template>
+                
+
+                  
+    <div>
+
+    </div>
+
+               
+                    
+
+               
+</template>
+
+<script>
+
+    
+
+    
+  export default {
+    
+    name: 'Modal-HomeContactUsS',
+    components: {
+        
+
+    },
+    data() {
+      return {
+
+        
+
+
+
+      }
+    },
+
+    methods:{
+      
+    }
+
+
+
+
+
+  }
+</script>
+
+
+<style lang="scss" scoped>
+  @import '../../assets/varmix.scss';
+    
+        
+
+		
+
+
+
+
+
+  
+</style>
---
+++ src/components/Modals/ModalContacUstMain.vue
@@ -0,0 +1,61 @@
+<template>
+                
+
+                  
+    <div>
+
+    </div>
+
+               
+                    
+
+               
+</template>
+
+<script>
+
+    
+
+    
+  export default {
+    
+    name: 'Modal-HomeContactUsMain',
+    components: {
+        
+
+    },
+    data() {
+      return {
+
+        
+
+
+
+      }
+    },
+
+    methods:{
+      
+    }
+
+
+
+
+
+  }
+</script>
+
+
+<style lang="scss" scoped>
+  @import '../../assets/varmix.scss';
+    
+        
+
+		
+
+
+
+
+
+  
+</style>
