5d2d6a9e97c0d71468a6424cc22e65de88a5b8d2
Oleh Khrystofor <hristoforolegivanovich@gmail.com>
2023-02-12 17:08:19 +0200

    add

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
 import CatalogView from '../views/CatalogView.vue'
+import VideoView from '../views/VideoView.vue.vue'
 import CarPage from '../views/CarPage.vue'
 
 const routes = [
@@ -33,6 +34,15 @@ const routes = [
     path: '/catalog/carpage',
     name: 'carpage',
     component: CarPage
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    // component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/video',
+    name: 'video',
+    component: VideoView
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
---
+++ src/views/VideoView.vue
@@ -0,0 +1,78 @@
+<template>
+  
+    <div class="d-flex flex-column min-vh-100">
+      <div class="container">
+        <div class="row">
+            <div class="col header" @clearmagtopform="setMagtopformInventoryComponent">
+              <Headerone  class="center" :liclickedtextoff="navliclicktextoffstate"  />
+              <burgerbtn v-bind:iscross="iscross" v-on:click="iscross=!iscross"  />
+              <transition name="menu">
+                <burgerlist v-if="iscross" class="burgerlist" :listate="navliclicktextoffstate" v-on:click="iscross=!iscross" v-on:navliclicktextoffevent="changenavliclicktextoffstate"  />
+              </transition>
+            </div>
+        </div>
+      </div>
+          
+      <main class="flex-fill">
+      
+       
+        
+  
+        
+  
+        
+        
+  
+      </main>
+  
+      <Footer class="center" />
+  
+    </div>
+       
+  </template>
+  
+  
+  
+  <script>
+  
+    
+  
+    export default {
+              components: {
+              
+              },
+              data() {
+                return {
+                 
+                  
+  
+                  
+  
+                }
+             },
+            methods: {
+  
+              
+            },
+  
+            watch: {
+             
+            },
+  
+    
+    
+    } 
+  
+  
+    
+  
+  </script>
+  
+  <style lang="scss" scoped>
+    @import '../assets/varmix.scss';
+  
+    
+    
+    
+  
+  </style>
